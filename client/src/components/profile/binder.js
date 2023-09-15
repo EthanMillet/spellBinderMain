d7e4c3226e4aba2c81f3bf6c89a2a84a6e29453a
Ethan <ethanamillet@gmail.com>
2023-09-15 11:47:36 -0600

    added bare bones forms for submitting maps and notes

--- client/src/components/profile/binder.js
+++ client/src/components/profile/binder.js
@@ -5,7 +5,7 @@ import { useQuery } from '@apollo/client';
 import { Link, useLocation } from 'react-router-dom';
 
 import { GET_BINDER, GET_USER, GET_MAP, GET_NOTE } from '../../utils/queries';
-
+import { ADD_NOTE, ADD_MAP } from '../../utils/mutations';
 
 
 
@@ -14,22 +14,116 @@ function BinderStation() {
 const location = useLocation()
 const { from } = location.state
 
+// access mutations
+const [addMap] = useMutation(ADD_MAP);
+const [addNote] = useMutation(ADD_NOTE);
+
+
+// set State for map and note forms
+const [mapFormState, setMapFormState] = useState({name: '', imageUrl: ''});
+const [noteFormState, setNoteFormState] = useState({title: '', content: ''});
+
 // load correct binder info
 const {loading, error, data } = useQuery(GET_BINDER, {
     variables: { _id: from },
 });
     if(loading) return "Loading..."
     if(error) return `${error.message}`;
-    console.log(data)
-return (
-// create map
-// create note
-// access map
-// access note
+
+    // create map 
+    const handleMapFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await addMap({
+                variables: {
+                    name: mapFormState.name, imageUrl: mapFormState.imageUrl}
+            });
+        } catch (e) {
+            console.log(e)
+        }
+    };
+
+    const handleMapFormChange = (event) => {
+        const { name, value } = event.target;
+        setMapFormState({
+            ...mapFormState,
+            [name]: value
+        });
+    };
+
+    // create note 
+    const handleNoteFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await addNote({
+                variables: {
+                    title: noteFormState.name, content: noteFormState.content}
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const handleNoteFormChange = (event) => {
+        const { name, value } = event.target;
+        setNoteFormState({
+            ...mapFormState,
+            [name]: value
+        });
+    }
+
+return (    
+    <div>
     <div>
         <h1>{ data.binder.name }</h1>
         <h2>{from}</h2>
     </div>
+
+{/* create map */}
+<form onSubmit={handleMapFormSubmit}>
+<label htmlFor="name">name</label>
+    <input
+        placeholder='name'
+        name='name'
+        type='name'
+        id='name'
+        onChange={handleMapFormChange}/>
+
+    <label htmlFor='imageUrl'>imageUrl</label>
+    <input 
+        placeholder='imageUrl'
+        name='imageUrl'
+        type='imageUrl'
+        id='imageUrl'
+        onChange={handleMapFormChange}/>
+
+<button type="submit">Submit</button>
+</form>
+
+{/* create note */}
+<form onSubmit={handleNoteFormSubmit}>
+    <label htmlFor="title">Title</label>
+    <input
+        placeholder='Note Title'
+        name='title'
+        type='title'
+        id='title'
+        onChange={handleNoteFormChange}/>
+
+    <label htmlFor='content'>Content</label>
+    <input 
+        placeholder='Content'
+        name='content'
+        type='content'
+        id='content'
+        onChange={handleNoteFormChange}/>
+
+<button type="submit">Submit</button>
+</form>
+
+{/* access map */}
+{/* access note */}
+</div>
 )
 
 }
