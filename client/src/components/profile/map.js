96eff4b179b5ad13e6700bd404082c700b86270b
Ethan <ethanamillet@gmail.com>
2023-11-08 13:14:07 -0700

    added custom image map function

--- client/src/components/profile/map.js
+++ client/src/components/profile/map.js
@@ -6,25 +6,70 @@ import { useQuery } from '@apollo/client';
 import { Link, useLocation } from 'react-router-dom';
 
 import { GET_MAP } from '../../utils/queries';
-import { ADD_MAP } from '../../utils/mutations';
 
-import { MapContainer, TileLayer } from 'react-leaflet'
+
+import {   MapContainer,
+    ZoomControl,
+    TileLayer,
+    GeoJSON,
+    ImageOverlay } from 'react-leaflet'
 
 import './map.css'
 import "leaflet/dist/leaflet.css"
 
 function CreateMapStation() {
+        const location = useLocation()
+        const { from } = location.state
 
 
-        const position = [51.505, -0.09]
+        const { loading, error, data } = useQuery(GET_MAP, {
+                variables: {_id: from},
+        });
+
+        if(loading) return "Loading..."
+        if(error) return `${error.message}`;
+
+        const position = [0, 0]
+
+        const IMG = new Image()
+        IMG.src = data.map.imageUrl;
+
+        IMG.onLoad = () => {
+            console.log(IMG.height)
+            console.log(IMG.width)
+        }
+
+
+        const bounds = [
+            [34, 9],
+            [32, 11]
+          ];
 
     return (
-        <MapContainer center={position} zoom={13}>
-                <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        <div>
+            <div>
+                <h1>{ data.map._id}</h1>
+                <h2>{ data.map.name }</h2>
+                <h2>{ data.map.imageUrl }</h2>
+                <h2>{from}</h2>
+            </div>
+
+        <MapContainer
+        center={[33, 9]}
+        zoom={6}
+        maxZoom={10}
+
+        zoomControl={false}
+        >
+        <ZoomControl position="bottomleft" />
+        
+
+                <ImageOverlay
+                        url={data.map.imageUrl}
+                        bounds={bounds}
                 />
         </MapContainer>
+        </div>
     )
 }
 
