4ef6aff5052fac47fea718779f569e2209a7b3d2
Ethan <ethanamillet@gmail.com>
2023-11-09 17:41:21 -0700

    adding stuff

--- client/src/components/profile/map.js
+++ client/src/components/profile/map.js
@@ -17,10 +17,43 @@ import {   MapContainer,
 import './map.css'
 import "leaflet/dist/leaflet.css"
 
+
+const M = ({ width, height, zoom, center, data }) => {
+    const wh = [width, 200];
+    const origin = [0, 0];
+    const bounds = [origin, wh];
+  
+    return (
+      <div style={{ width: "70%", height: "100vh" }}>
+        <MapContainer
+          style={{ height: "100%", minHeight: "100%" }}
+          bounds={zoom ? undefined : bounds}
+          boundsOptions={{
+            padding: [0, 0]
+          }}
+          maxBounds={bounds}
+          zoom={center ? zoom : undefined}
+          center={zoom ? center : undefined}
+          zoomSnap={0} // Important to disable snap after fitBounds
+          whenReady={(e) => e.target.fitBounds(bounds)} // Have the map adjust its view to the same bounds as the Image Overlay
+        >
+          <ImageOverlay
+            url={data}
+            bounds={bounds}
+            className="map_main"
+          />
+        </MapContainer>
+      </div>
+    );
+  };
+  
+
 function CreateMapStation() {
         const location = useLocation()
         const { from } = location.state
-
+        
+        const [mapWidthSize, setMapWidthSize] = useState()
+        const [mapHeightSize, setMapHeightSize] = useState()
 
         const { loading, error, data } = useQuery(GET_MAP, {
                 variables: {_id: from},
@@ -33,17 +66,11 @@ function CreateMapStation() {
 
         const IMG = new Image()
         IMG.src = data.map.imageUrl;
-
-        IMG.onLoad = () => {
-            console.log(IMG.height)
-            console.log(IMG.width)
+        IMG.onload = () => {
+            setMapWidthSize(IMG.width)
+            setMapHeightSize(IMG.height)
         }
 
-
-        const bounds = [
-            [34, 9],
-            [32, 11]
-          ];
 
     return (
         <div>
@@ -54,21 +81,7 @@ function CreateMapStation() {
                 <h2>{from}</h2>
             </div>
 
-        <MapContainer
-        center={[33, 9]}
-        zoom={6}
-        maxZoom={10}
-
-        zoomControl={false}
-        >
-        <ZoomControl position="bottomleft" />
-        
-
-                <ImageOverlay
-                        url={data.map.imageUrl}
-                        bounds={bounds}
-                />
-        </MapContainer>
+            <M width={IMG.width} height={IMG.height} center={[0, 0]} data={data.map.imageUrl} />
         </div>
     )
 }
