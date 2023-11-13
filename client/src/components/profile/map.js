4e3ce88a54e2faf26b5094e70ec1195e655185d0
Ethan <ethanamillet@gmail.com>
2023-11-13 15:45:45 -0700

    map now functions as intended

--- client/src/components/profile/map.js
+++ client/src/components/profile/map.js
@@ -7,10 +7,79 @@ import { Link, useLocation } from 'react-router-dom';
 import { ADD_TOKEN } from '../../utils/mutations';
 import { GET_MAP } from '../../utils/queries';
 import { MapContainer, useMapEvents, Marker, Popup, ImageOverlay } from 'react-leaflet'
-
+import L from 'leaflet';
 import './map.css'
 import "leaflet/dist/leaflet.css"
 
+const capitalIcon = new L.icon({
+  iconUrl: require('../../assets/images/Capital.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const caveIcon = new L.icon({
+  iconUrl: require('../../assets/images/Cave.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const dockIcon = new L.icon({
+  iconUrl: require('../../assets/images/dock.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const dungeonIcon = new L.icon({
+  iconUrl: require('../../assets/images/Dungeon.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const lighthouseIcon = new L.icon({
+  iconUrl: require('../../assets/images/lighthouse.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const millIcon = new L.icon({
+  iconUrl: require('../../assets/images/mill.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const templeIcon = new L.icon({
+  iconUrl: require('../../assets/images/Temple.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const townIcon = new L.icon({
+  iconUrl: require('../../assets/images/Town.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const villageIcon = new L.icon({
+  iconUrl: require('../../assets/images/Village.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
+
+const waterfallIcon = new L.icon({
+  iconUrl: require('../../assets/images/Waterfall.png'),
+  iconSize: [46.2,46.2],    
+  iconAnchor: [22, 35],
+  popupAnchor: [0, 0]
+})
 
 const M = ({ width, height, zoom, center, data }) => {
     const [positionState, setPositionState] = useState()
@@ -67,7 +136,9 @@ const M = ({ width, height, zoom, center, data }) => {
     const location = useLocation()
     const { from } = location.state
 
+    const [selectedOption, setSelectedOption] = useState();
     const [tokenFormState, setTokenFormState] = useState({title: '', content: '', img: ''})  
+    
     const [addToken] = useMutation(ADD_TOKEN);
 
 
@@ -76,7 +147,7 @@ const M = ({ width, height, zoom, center, data }) => {
       try {
           await addToken({
               variables: {
-                  position: "[73.8989223036342, 143.83317663962055]", title: tokenFormState.title, content: tokenFormState.content, tokenImg: "test", mapID: from}
+                  position: `${position.position.lat}, ${position.position.lng}`, title: tokenFormState.title, content: tokenFormState.content, tokenImg: selectedOption, mapID: from}
           });
       } catch (e) {
           console.log(e);
@@ -91,6 +162,8 @@ const M = ({ width, height, zoom, center, data }) => {
       })
   }
 
+
+
     return(
 <div>
       <div className='modal-header'>
@@ -99,7 +172,7 @@ const M = ({ width, height, zoom, center, data }) => {
 
       <div className='modal-body'>
 <form className='form-body' onSubmit={handleTokenFormSubmit}>
-    <label className='label' htmlFor="name">Name</label>
+    <label className='label' htmlFor="name">Title</label>
         <input
         placeholder='title'
         name='title'
@@ -107,7 +180,7 @@ const M = ({ width, height, zoom, center, data }) => {
         id='title'
             onChange={handleTokenFormChange}/>
             <hr className='form-break'></hr>
-    <label className='label' htmlFor='imageUrl'>imageUrl</label>
+    <label className='label' htmlFor='imageUrl'>Content</label>
         <input 
         placeholder='Content'
         name='content'
@@ -115,6 +188,25 @@ const M = ({ width, height, zoom, center, data }) => {
         id='content'
         onChange={handleTokenFormChange}/>
         <hr className='form-break'></hr>
+
+    <label className='label' htmlFor='imageType'>Icon</label>
+      <select value={selectedOption}
+        onChange={e => setSelectedOption(e.target.value)}>
+        <option value={'Captial'}>Capital</option>
+        <option value={'Cave'}>Cave</option>
+        <option value={'Dock'}>Dock</option>
+        <option value={'Dungeon'}>Dungeon</option>
+        <option value={'Lighthouse'}>Lighthouse</option>
+        <option value={'Mill'}>Mill</option>
+        <option value={'Temple'}>Temple</option>
+        <option value={'Town'}>Town</option>
+        <option value={'Village'}>Village</option>
+        <option value={'Waterfall'}>Waterfall</option>
+      </select>
+
+        <hr className='form-break'></hr>
+
+
     <button className="sumbit-button" type="submit">Submit</button>
 </form>
       </div>
@@ -134,13 +226,38 @@ const M = ({ width, height, zoom, center, data }) => {
     if(loading) return "Loading..."
     if(error) return `${error.message}`;
 
+    const renderIMG = (token) => {
+      if (token === "Capital") {
+        return capitalIcon
+      } else if (token === "Cave") {
+        return caveIcon
+      } else if (token === "Dock") {
+        return dockIcon
+      } else if (token === "Dungeon") {
+        return dungeonIcon
+      } else if (token === "Lighthouse") {
+        return lighthouseIcon
+      } else if (token === "Mill") {
+        return millIcon
+      } else if (token === "Temple") {
+        return templeIcon
+      } else if (token === "Town") {
+        return townIcon
+      } else if (token === "Village") {
+        return villageIcon
+      } else {
+        return waterfallIcon
+      }
+    }
+
     return (
       <div>
 
       {data.map.tokens?.map((token) => (
         
-        <Marker key={token._id} position={[62.3295853023709 , 109.98921765300834]}>
-          {console.log(token)}
+        <Marker key={token._id} position={JSON.parse("[" + token.position + "]")} 
+        icon={renderIMG(token.tokenImg)}>
+
           <Popup>
             <h2>{token.title}</h2>
             <p>{token.content}</p>
